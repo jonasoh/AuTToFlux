7a35721f82629b88227535beff38923f2914073e
Jonas Ohlsson <jonas@vansinne.se>
2019-02-27 15:06:28 +0100

    Be more flexible with file names.

--- Control-vs-Reporter.R
+++ Control-vs-Reporter.R
@@ -79,9 +79,9 @@ for(d in realdirs) {
     
     # check that the name conforms to naming standard
     # if it doesn't conform, we stop the script to avoid calculation errors
-    stopifnot(grepl('^[[:print:]]+_[[:print:]]+_seedling[[:alnum:]]+_image[[:alnum:]]+\\.[[:alnum:]]{2,4}\\.csv$', bname))
+    stopifnot(grepl('^[[:print:]]+_[[:print:]]+_seedling[[:alnum:]]+_image[[:alnum:]]+(\\.[[:alnum:]]{2,4})?\\.[[:alnum:]]{2,4}\\.csv$', bname))
     
-    bname <- sub('\\.[[:alnum:]]{2,4}\\.csv', '', bname, fixed = FALSE)
+    bname <- sub('\\.[[:alnum:]]{2,4}(\\.[[:alnum:]]{2,4})?\\.csv', '', bname, fixed = FALSE)
     
     params <- unlist(strsplit(bname, '_', fixed = TRUE))
     
--- Flux-vs-Time.R
+++ Flux-vs-Time.R
@@ -70,9 +70,9 @@ for(f in files) {
   
   # check that the name conforms to naming standard
   # if it doesn't conform, we stop the script to avoid calculation errors
-  stopifnot(grepl('^[[:print:]]+_[[:print:]]+_seedling[[:alnum:]]+_image[[:alnum:]]+\\.[[:alnum:]]{2,4}\\.csv$', bname))
+  stopifnot(grepl('^[[:print:]]+_[[:print:]]+_seedling[[:alnum:]]+_image[[:alnum:]]+(\\.[[:alnum:]]{2,4})?\\.[[:alnum:]]{2,4}\\.csv$', bname))
   
-  bname <- sub('\\.[[:alnum:]]{2,4}\\.csv', '', bname, fixed = FALSE)
+  bname <- sub('\\.[[:alnum:]]{2,4}(\\.[[:alnum:]]{2,4})?\\.csv', '', bname, fixed = FALSE)
   
   params <- unlist(strsplit(bname, '_', fixed = TRUE))
   
